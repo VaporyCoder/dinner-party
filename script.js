4f7458e49b4e1d97592821e31989c3b28438a526
Jon <jlara2019@gmail.comgit config --global user.email jlara2019@gmail.compwdgit commit>
2023-08-18 11:44:11 -0500

    solved

---
+++ script.js
@@ -0,0 +1,25 @@
+function sumValues(obj){
+    let sum = 0;
+    for(let key in obj) {
+        sum = sum + obj[key];
+    }
+    return sum;
+}
+
+const dinner = {
+    cheeseburger: 12,
+    steak: 20,
+    soup: 8,
+    macAndCheese: 14,
+    soupAndSalad: 16
+};
+
+let sum = 0;
+for(let key in dinner) {
+    console.log(key);
+    console.log(dinner[key]);
+    sum = sum + dinner[key];
+}
+console.log(sumValues(dinner));
+
+console.log(sum);
